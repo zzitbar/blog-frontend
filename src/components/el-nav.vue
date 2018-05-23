4f3df82bf95caeedf78380a3f293d6b282f91186
greenman0007 <greenman0007@gmail.com>
2018-05-23 14:01:14 +0800

    导航栏修改

--- src/components/el-nav.vue
+++ src/components/el-nav.vue
@@ -14,48 +14,19 @@
                 <span slot="title">首页</span>
             </el-menu-item>
             <el-submenu index="1">
-                <template slot="title"><i class="el-icon-message"></i> <span v-if="!childCollapse">导航一</span></template>
+                <template slot="title"><i class="el-icon-message"></i> <span v-if="!childCollapse">文章管理</span></template>
+                <el-menu-item index="/form">表单</el-menu-item>
+                <el-menu-item index="/aitlcle">文章列表</el-menu-item>
+                <el-menu-item index="/user/password">分类管理</el-menu-item>
+                <el-menu-item index="/user/password">标签管理</el-menu-item>
+            </el-submenu>
+            <el-submenu index="2">
+                <template slot="title"><i class="el-icon-menu"></i><span v-if="!childCollapse">用户管理</span></template>
                 <el-menu-item-group>
                     <template slot="title">用户</template>
                     <el-menu-item index="/user/account">用户管理</el-menu-item>
                     <el-menu-item index="/user/password">修改密码</el-menu-item>
                 </el-menu-item-group>
-                <el-menu-item-group title="表单">
-                    <el-menu-item index="/form">表单</el-menu-item>
-                </el-menu-item-group>
-                <el-menu-item-group title="文章管理">
-                    <el-menu-item index="/aitlcle">文章管理</el-menu-item>
-                </el-menu-item-group>
-            </el-submenu>
-            <el-submenu index="2">
-                <template slot="title"><i class="el-icon-menu"></i><span v-if="!childCollapse">导航二</span></template>
-                <el-menu-item-group>
-                    <template slot="title">分组一</template>
-                    <el-menu-item index="2-1">选项1</el-menu-item>
-                    <el-menu-item index="2-2">选项2</el-menu-item>
-                </el-menu-item-group>
-                <el-menu-item-group title="分组2">
-                    <el-menu-item index="2-3">选项3</el-menu-item>
-                </el-menu-item-group>
-                <el-submenu index="2-4">
-                    <template slot="title">选项4</template>
-                    <el-menu-item index="2-4-1">选项4-1</el-menu-item>
-                </el-submenu>
-            </el-submenu>
-            <el-submenu index="3">
-                <template slot="title"><i class="el-icon-setting"></i><span v-if="!childCollapse">导航三</span></template>
-                <el-menu-item-group>
-                    <template slot="title">分组一</template>
-                    <el-menu-item index="3-1">选项1</el-menu-item>
-                    <el-menu-item index="3-2">选项2</el-menu-item>
-                </el-menu-item-group>
-                <el-menu-item-group title="分组2">
-                    <el-menu-item index="3-3">选项3</el-menu-item>
-                </el-menu-item-group>
-                <el-submenu index="3-4">
-                    <template slot="title">选项4</template>
-                    <el-menu-item index="3-4-1">选项4-1</el-menu-item>
-                </el-submenu>
             </el-submenu>
         </el-menu>
     </div>
